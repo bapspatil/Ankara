c660af0a8a026dab07ba454f3573f68f4651405d
Bapusaheb Patil <bapoo1998@gmail.com>
2018-02-16 23:16:32 +0530

    Reformatted code

--- app/src/main/kotlin/bapspatil/examples/ankara/MainView.kt
+++ app/src/main/kotlin/bapspatil/examples/ankara/MainView.kt
@@ -21,17 +21,18 @@ class MainView : AnkoComponent<MainActivity> {
             textView {
                 id = R.id.helloTextView // ID for TextView so it can referenced later
                 text = context.getString(R.string.hello) // Text for TextView
-                textSize = 16f  // Text size for TextView
-                textColor = 0xffffff.opaque
-                padding = dip(16)
-                gravity = Gravity.START
+                textSize = 32f  // Text size for TextView
+                textColor = 0xffffff.opaque // Text color for TextView
+                padding = dip(16) // Padding for TextView
+                gravity = Gravity.START // Gravity for TextView (gravity)
             }.lparams(width = wrapContent, height = wrapContent) {
-                gravity = Gravity.CENTER
+                gravity = Gravity.CENTER // Layout Gravity for TextView (layout_gravity)
+                margin = dip(4) // Margin for TextView
             }
 
             // Button with Anko Layouts
             button("Toast") {
-                onClick {
+                onClick { // On Click listener for Button
                     // Toast with Anko Commons
                     toast("You just clicked me!")
                 }
@@ -40,9 +41,9 @@ class MainView : AnkoComponent<MainActivity> {
                 margin = dip(8)
             }
 
+            // AlertDialog with Anko Commons
             button("Alert Dialog") {
                 onClick {
-                    // AlertDialog with Anko Commons
                     alert("Did you just click me?", "Hey, you.") {
                         yesButton { toast("You did click me!") }
                         cancelButton { toast("Sure you didn't?") }
@@ -53,16 +54,36 @@ class MainView : AnkoComponent<MainActivity> {
                 margin = dip(8)
             }
 
+            // Selector with Anko Commons
             val thingsYouLove = listOf("Kotlin", "Anko", "Karamba", "Ankara", "BlrKotlin")
             button("Selector") {
                 onClick {
-                    // Selector with Anko Commons
                     selector("What do you love?", thingsYouLove, { dialogInterface, i ->
                         toast("I knew you loved ${thingsYouLove[i]}!")
 
                         // Logging with Anko Commons
                         v(this@MainView.javaClass.simpleName, "CLICKED: ${thingsYouLove[i]}")
                     })
+                }
+            }.lparams(width = wrapContent, height = wrapContent) {
+                gravity = Gravity.CENTER
+                margin = dip(8)
+            }
+
+            // Browser Intent with Anko Commons
+            button("Visit My Website") {
+                onClick {
+                    browse("https://bapspatil.com")
+                }
+            }.lparams(width = wrapContent, height = wrapContent) {
+                gravity = Gravity.CENTER
+                margin = dip(8)
+            }
+
+            // Share Intent with Anko Commons
+            button("Share This App") {
+                onClick {
+                    share("Hey, check out this example app for Anko & Karamba, developed by Bapusaheb Patil at:\nhttps://github.com/bapspatil/Ankara", "Share Ankara's GitHub link")
                 }
             }.lparams(width = wrapContent, height = wrapContent) {
                 gravity = Gravity.CENTER
